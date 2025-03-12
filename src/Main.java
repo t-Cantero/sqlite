854b9b7001ae1a3c95f1e8d6034c424c069422b9
tcanterorodriguez <antcanrod@alu.edu.gva.es>
2025-03-12 19:31:49 +0100

    Salida del main y retorno al main añadido

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,17 @@ public class Main {
         do{
             System.out.println("Sobre que tabla deseas actuar:");
             System.out.println("1.Libros");
+            System.out.println("2.Salir");
             n = Utilidades.leerEntero("Elige una opción (introduzca el número):");
-        }while(n!=1);//Provisional
-
-        switch (n){
-            case 1://Mostrar opciones sobre la tabla Libros
-                opcionesLibros();
-                break;
+        }while(n<1 || n>2);//Provisional
+        if(n == 2){
+            System.out.println("Salida realizada con éxito");
+        }else{
+            switch (n){
+                case 1://Mostrar opciones sobre la tabla Libros
+                    opcionesLibros();
+                    break;
+            }
         }
     }
 
@@ -28,11 +32,11 @@ public class Main {
             System.out.println("2. Agregar Libro");
             System.out.println("3. Eliminar Libro");
             System.out.println("4. Modificar Disponibilidad");
-            System.out.println("5. Salir");
+            System.out.println("5. Volver a la página inicial");
             n = Utilidades.leerEntero("Elige una opción  (Introduzca el número):");
         }while(n<1 || n>5);
         if(n==5){
-            System.out.println("Saliste con éxito");
+            mostrarOpciones();
         }else{
             switch(n){//Prueba Inicial
                 case 1://Listar Libro
